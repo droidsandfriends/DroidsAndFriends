0db71cb9e463915c58a4aa85b0eb4c17186400dc
Attila Bodis <attila@google.com>
2014-07-23 16:13:54 -0700

    Dynamic pricing for photos, communications, and skid pad

--- src/com/droidsandfriends/EventServlet.java
+++ src/com/droidsandfriends/EventServlet.java
@@ -78,18 +78,21 @@ public class EventServlet extends HttpServlet {
 
       List<LineItem> expenses = new ArrayList<>();
       expenses.add(new LineItem("Track rental", (isFiveMile ? 8000 : 4500) * 100, 1));
+      if (isFiveMile) {
+        expenses.add(new LineItem("Skid pad rental", 350 * 100, 1));
+      }
       expenses.add(new LineItem("Insurance", 1275 * 100, 1));
       expenses.add(new LineItem("ALS ambulance (per hour)", (isFiveMile ? 220 : 155) * 100, 8));
       expenses.add(new LineItem("Event control", 250 * 100, 1));
       expenses.add(new LineItem("Flaggers", 135 * 100, flaggerCount));
-      expenses.add(new LineItem("Communications service", 250 * 100, 1));
+      expenses.add(new LineItem("Communications line", 250 * 100, (isFiveMile ? 2 : 1)));
       expenses.add(new LineItem("PA system", 325 * 100, 1));
       expenses.add(new LineItem("Radio rental", 30 * 100, 1));
       expenses.add(new LineItem("Tow standby ($600 if used)", 175 * 100, 1));
       expenses.add(new LineItem("Fire/emergency standby ($600 if used)", 175 * 100, 1));
       expenses.add(new LineItem("Sanitary service", 250 * 100, 1));
       expenses.add(new LineItem("Electrical service", 150 * 100, 1));
-      expenses.add(new LineItem("Photography", 1500 * 100, 1));
+      expenses.add(new LineItem("Photography", (isFiveMile ? 1875 : 1500) * 100, 1));
       expenses.add(new LineItem("Instructors", 53 * 100, instructorCount));
       expenses.add(new LineItem("Catering", 28 * 100, (flaggerCount + instructorCount + driverCount + guestCount + 2)));
       request.setAttribute("expenses", expenses);
